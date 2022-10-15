df65fb98e1f371a0a168334463db40c76fa37b98
lizakhmyz <lizamari21@gmail.com>
2022-10-15 18:41:21 +0200

    fixed cringe

--- src/lab3/shapes/quadrangles/Quadrangle.java
+++ src/lab3/shapes/quadrangles/Quadrangle.java
@@ -75,7 +75,7 @@ public abstract class Quadrangle extends Shape {
     public static boolean collinearityOfThreePoints(double x1, double y1,
                                                     double x2, double y2,
                                                     double x3, double y3) {
-        return ((x3 - x1) / (x2 - x1) == (y3 - y1) / (y2 - y1));
+        return ((y2 - y1) / (x3 - x1) == (y3 - y1) / (x2 - x1));
     }
 
     @Override
